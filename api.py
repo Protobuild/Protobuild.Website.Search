56ac4e114720b8ded66dfb83a323d685a1cc398d
James Rhodes <jrhodes@redpointsoftware.com.au>
2014-11-14 17:34:27 +1100

    Attempt to store documents in search API

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import webapp2
 from google.appengine.ext import ndb
+from google.appengine.api import search
 
 class package(ndb.Model):
     name = ndb.StringProperty(required=True);
@@ -21,12 +22,36 @@ class ReindexPackage(webapp2.RequestHandler):
             self.response.write("No such package")
             return
 
-        self.response.write(package.name + "\n")
-        self.response.write(package.description + "\n")
-        pass
+        index = search.Index(name='names-and-descriptions')
+
+        fields = [
+            search.TextField(name='name', value=package.name),
+            search.TextField(name='description', value=package.description),
+        ]
+
+        document = search.Document(doc_id=str(package_id), fields=fields)
+
+        try:
+            index.put(document)
+            self.response.write("Package re-indexed successfully")
+        except search.Error:
+            self.response.write("Package failed to be indexed")
+
+class SearchPackage(webapp2.RequestHandler):
+    def get(self, query):
+        self.response.headers['Content-Type'] = 'application/json'
+
+        index = search.Index(name='names-and-descriptions')
+        try:
+            search_query = search.Query(query_string=query)
+            search_results = index.search(search_query)
+            self.response.write('{"error": false, "results": []}')
+        except search.Error:
+            self.response.write('{"error": true, "results": []}')
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/reindex/([0-9]+)', ReindexPackage),
+    ('/search/(.+)', SearchPackage),
 ], debug=True)
 
